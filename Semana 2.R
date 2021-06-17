fa8f4a5fda104902c0e8cd0517270fd1210812a8
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-06-16 18:55:46 -0600

    Update Semana 2.R
    practica

--- Semana 2.R
+++ Semana 2.R
@@ -1,2 +1,61 @@
-plot(iris$Species)
-Hola
+#Práctica 1
+
+#Realice un ejemplo de las estructuras if y for, 
+#utilice la cantidad de datos diferentes de
+#las que se utilizaron en clase. 
+edad = 89
+if (edad >=18) {
+  print("Mayor de edad")
+}else{
+  print("Menor de edad")
+}
+
+#Declare un tipo de variable double,
+#integer y caracter 
+var1 = 2L
+var2=4.8
+var3="Caracter"
+
+typeof()
+class()
+
+#Cree un vector de enteros 
+vectorEnteros = c(2L,3L,4L,5L)
+
+#Cree un vector con los meses del año  
+vectorMeses = c("Enero","Febrero","Marzo","Abril")
+#Cree un vector con nombre de personas  
+vectorMeses = c("Ana","Juan","Gerardo","Abril")
+#Cree los siguientes vectores  
+vectorPelicula = c("Sherk","Sherk 2","Sherk tercero","Sherk Felices")
+VectorPuntuacion=c(7.9,7.2,6.1,6.3)
+VectorEstreno = c(TRUE,TRUE,FALSE,FALSE)
+
+#Saque las siguientes funciones con base 
+#algún vector numérico del ejercicio 6
+
+length(VectorPuntuacion)
+min(VectorPuntuacion)
+max(VectorPuntuacion)
+sum(VectorPuntuacion)
+mean(VectorPuntuacion)
+median(VectorPuntuacion)
+sort(VectorPuntuacion)
+plot(VectorPuntuacion)
+unique(VectorPuntuacion)
+which.max(VectorPuntuacion)
+which.min(VectorPuntuacion)
+sqrt(VectorPuntuacion)
+
+#Genere un vector con valores del 1 al 100 
+
+vectorSeq=c(1:100)
+
+#Genere un vector con valores 
+#del 1 al 100 de 10 en 10 
+vectroSeqBy = seq(  0, 100,10)
+#seq(de, hasta, por)
+print(vectroSeqBy)
+vectroSeqBy
+
+
