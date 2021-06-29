e59f16463194fbbb921c3925ce4ee4f3d83f1326
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-06-28 19:21:12 -0600

    Update Semana 4.R

--- Semana 4.R
+++ Semana 4.R
@@ -10,7 +10,19 @@ hist(x=Netflix$`IMDB Score`,
 
 #Plots
 
-#Relación y correlación 
-
+#Relación y correlación
+#Dispersión
 plot(Netflix$Runtime,Netflix$`IMDB Score`)
+
+#Grafico de barras
+dataNetflixFiltrada = filter(Netflix, `IMDB Score`>6  & `IMDB Score`<6.5)
+
+plot(as.factor(dataNetflixFiltrada$Genre))
+
+#Time based plot
+
+mis_fechas <- seq(as.Date("2005/1/1"), by = "month", length = 50  mi_factor <- factor(mtcars$cyl)
+                  
+
+
 
