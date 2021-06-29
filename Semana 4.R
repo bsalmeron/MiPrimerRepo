937b07513b33cceefb71e583b81101f2bc741e7b
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-06-28 18:43:38 -0600

    Create Semana 4.R

---
+++ Semana 4.R
@@ -0,0 +1,20 @@
+#Histogramas
+#Analisis exploratorio 1- Qué quiero
+
+hist(x=Netflix$`IMDB Score`, 
+     main = "Histograma IMDB Score - Netflix", 
+     xlab = "Score", 
+     ylab = "Frecuencia",
+     col = "red")
+
+
+
+
+
+
+
+
+
+
+
+#Plots
