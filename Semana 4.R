b9ad2f83acf1e3ae9904ce687c2338899f4f4e68
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-06-28 20:30:02 -0600

    Update Semana 4.R

--- Semana 4.R
+++ Semana 4.R
@@ -15,7 +15,7 @@ hist(x=Netflix$`IMDB Score`,
 plot(Netflix$Runtime,Netflix$`IMDB Score`)
 
 #Grafico de barras
-dataNetflixFiltrada = filter(Netflix, `IMDB Score`>6  & `IMDB Score`<6.5)
+dataNetflixFiltrada = filter(Netflix, Netflix$`IMDB Score`>6  & `IMDB Score`<6.5)
 
 plot(as.factor(dataNetflixFiltrada$Genre))
 
@@ -32,5 +32,57 @@ View(trees)
 plot(select(trees,1:3))
 
 
-plot(mis_fechas,rnorm(50), type = "s", main = "Hola")
+plot(mis_fechas,rnorm(50), type = "o", main = "Hola")
+
+#Ejemplo 
+
+t <- rep(seq(25, 5, -5), 5)
+
+r <- c(sapply(seq(5, 25, 5), function(i) rep(i, 5)))
+
+
+plot(r, t, pch = 1:25, cex = 3, yaxt = "n", xaxt = "n",  ann = FALSE, xlim = c(3, 27), lwd = 1:3)
+text(r - 1.5, t, 1:25)
+
+
+
+plot(mis_fechas,rnorm(50), type = "o", pch= 5, main = "Hola", col= "red",
+     cex=1, lwd= 1, sub = "Sub titulo", ylab = "Titulo y", xlab = "Titulo x",
+     ylim = c(0,3))
+
+#Funciones 
+
+#Sistema
+hist()
+plot()
+head()
+mean()
+min()
+max()
+
+#Funciones por el usuario
+nombre <- function(argumentos) {
+  IF(S){
+    #LOGICA
+  }ELSE{
+  }
+}
+
+
+
+Plot_Score <- function(score1,score2, idioma) {
+  dataNetflixFiltrada = filter(Netflix, `IMDB Score`>score1  &
+                                 `IMDB Score`<score2 &
+                                  Language==idioma)
+  plot(as.factor(dataNetflixFiltrada$Genre))
+  
+}
+
+Suma(1,10)
+
+
+Plot_Score(6,8,"Italian")
+
+
+
 
