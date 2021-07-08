20c63c94969bdca209522191ecfed115088df069
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-07-07 18:12:31 -0600

    Create Semana 5.R

---
+++ Semana 5.R
@@ -0,0 +1,18 @@
+library("DBI")
+library("odbc")
+library("dbplyr")
+
+DB_BikeStores <- DBI::dbConnect(odbc::odbc(),
+                        Driver = "SQL Server",
+                        Server = "dbtedamssql.uh.ac.cr",
+                        Database = "BikeStores",
+                        UID = rstudioapi::askForPassword("Database user"),
+                        PWD = rstudioapi::askForPassword("Database password"),
+                        Port = 1433)
+
+
+Stores<-dbGetQuery(DB_BikeStores,"select * from [sales].[order_items]")
+View(Stores)
+
+##Pentaho_User
+##Pentaho_User2021
