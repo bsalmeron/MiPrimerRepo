74460525c525e48b2cefa72ba36922a24b60c3e7
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2021-07-14 21:01:49 -0600

    Update Analsis de sentimiento.R

--- Analsis de sentimiento.R
+++ Analsis de sentimiento.R
@@ -36,3 +36,53 @@ length(oraciones_vector)
 #Paso 4 Extracción de datos con el Léxico de Sentimientos NRC
 
 sentimientos_df <- get_nrc_sentiment(texto_palabras, lang="spanish")
+
+View(sentimientos_df)
+
+summary(sentimientos_df)
+
+#Graficar 
+
+barplot(
+  colSums(prop.table(sentimientos_df[, 1:8])),
+  space = 0.2,
+  horiz = FALSE,
+  las = 1,
+  cex.names = 0.7,
+  col = brewer.pal(n = 8, name = "Set3"),
+  main = "Centro de contacto",
+  sub = "Análisis",
+  xlab="emociones", ylab = NULL)
+
+
+barplot(colSums(prop.table(sentimientos_df[, 1:8])))
+
+#Recuento de palabras 
+
+palabras_tristeza <- texto_palabras[sentimientos_df$sadness> 0]
+
+palabras_tristeza_orden <- sort(table(unlist(palabras_tristeza)), decreasing = TRUE)
+
+
+#Nube de emociones 
+
+nube_emociones_vector <- c(
+  paste(texto_palabras[sentimientos_df$sadness> 0], collapse = " "),
+  paste(texto_palabras[sentimientos_df$joy > 0], collapse = " "),
+  paste(texto_palabras[sentimientos_df$anger > 0], collapse = " "),
+  paste(texto_palabras[sentimientos_df$fear > 0], collapse = " "))
+
+nube_emociones_vector <- iconv(nube_emociones_vector, "latin1", "UTF-8")
+
+nube_corpus <- Corpus(VectorSource(nube_emociones_vector))
+
+nube_tdm <- TermDocumentMatrix(nube_corpus)
+nube_tdm <- as.matrix(nube_tdm)
+
+colnames(nube_tdm) <- c('tristeza', 'felicidad', 'enfado', 'confianza')
+head(nube_tdm)
+
+set.seed(757) # puede ser cualquier número
+comparison.cloud(nube_tdm, random.order = FALSE,
+                 colors = c("green", "red", "orange", "blue"),
+                 title.size = 1, max.words = 50, scale = c(2.5, 1), rot.per = 0.4)
